b744ab7c94c1df2a742f2a001d30e3a920958bcc
Calcium <108228113+eshan-one@users.noreply.github.com>
2022-08-21 12:08:35 +0530

    Create Program_6.c
    Sort the values in an array of size 10

---
+++ Program_6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+  int a[10];
+  int i,j,temp;
+  printf("Enter 10 numbers:\n");
+
+  for(i=0;i<=9;i++)
+    {
+      scanf("%d ", &a[i]);
+      
+    }
+
+  for(i=0;i<9;i++)
+    {
+      for(j=i+1;j<10;j++)
+        {
+          if(a[i]>a[j])
+          {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            
+            
+          }
+        }
+    }
+
+  for(i=0;i<=9;i++)
+    {
+      printf(" %d ", a[i]);
+    }
+
+  
+  return 0;
+}
+
+
