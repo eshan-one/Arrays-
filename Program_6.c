e37d4ce34e19d192cd7080a0d59533954c072bb9
Calcium <108228113+eshan-one@users.noreply.github.com>
2022-08-21 12:09:33 +0530

    Update Program_6.c

--- Program_6.c
+++ Program_6.c
@@ -5,7 +5,7 @@ int main() {
   int i,j,temp;
   printf("Enter 10 numbers:\n");
 
-  for(i=0;i<=9;i++)
+  for(i=0;i<10;i++)
     {
       scanf("%d ", &a[i]);
       
@@ -26,7 +26,7 @@ int main() {
         }
     }
 
-  for(i=0;i<=9;i++)
+  for(i=0;i<10;i++)
     {
       printf(" %d ", a[i]);
     }
